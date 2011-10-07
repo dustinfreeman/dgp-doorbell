1968df303dfbf07086024c3022a5b2f5bcc8ef40
dustin.freeman@gmail.com <dustin.freeman@gmail.com@14a73957-cca7-8a38-00bf-a591336eb701>
2011-10-07 18:56:03 +0000

    refactor, mofo. Check in exe so it can be run easily.

---
+++ UserFrame.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DGPDoorbell
+{
+    /// <summary>
+    /// Interaction logic for UserFrame.xaml
+    /// </summary>
+    public partial class UserFrame : UserControl
+    {
+        //presents an interface to a single user.
+
+        public UserFrame()
+        {
+            InitializeComponent();
+        }
+    }
+}
