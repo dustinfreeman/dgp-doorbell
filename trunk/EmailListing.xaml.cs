4915b25037312ae1646c895bede969b826605f8d
dustin.freeman@gmail.com <dustin.freeman@gmail.com@14a73957-cca7-8a38-00bf-a591336eb701>
2011-10-07 19:51:27 +0000

    Have basic emaillistings....

---
+++ trunk/EmailListing.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DGPDoorbell
+{
+    /// <summary>
+    /// Interaction logic for EmailListing.xaml
+    /// </summary>
+    public partial class EmailListing : UserControl
+    {
+        string dgpID;
+        string emailAddress;
+        string GivenName;
+        string LastName;
+
+        public EmailListing()
+        {
+            InitializeComponent();
+        }
+
+        public void Update()
+        {
+            LastNameTxt.Text = LastName;
+            GivenNameTxt.Text = GivenName;
+            EmailTxt.Text = emailAddress;
+        }
+
+        public static EmailListing EmailListingFactory(string line)
+        {
+            try
+            {
+                string[] lineSplit = line.Split(',');
+
+                EmailListing eListing = new EmailListing();
+
+                eListing.dgpID = lineSplit[0];
+                eListing.emailAddress = lineSplit[1];
+                eListing.GivenName = lineSplit[2];
+                eListing.LastName = lineSplit[3];
+
+                eListing.Update();
+
+                return eListing;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
